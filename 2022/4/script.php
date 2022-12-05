1d56b8593b5935deacd8003c799f29c3bc272f5f
Ricardo Boss <contact@ricardoboss.de>
2022-12-05 11:17:29 +0100

    Finish day 4

--- 2022/4/script.php
+++ 2022/4/script.php
@@ -1,32 +1,24 @@
 <?php
 $input = file_get_contents("input.txt");
 $pairs = array_map(
-	fn (string $line) => explode(',', $line),
+	fn(string $line) => array_map(
+		fn(string $pair) => array_map(
+			fn(string $bound) => (int)$bound,
+			explode("-", $pair),
+		),
+		explode(',', $line),
+	),
 	array_filter(explode("\n", $input)),
 );
+unset($input);
 
 $redundant = 0;
 foreach ($pairs as $pair) {
-	assert(count($pair) === 2);
-
-	$pair1 = explode("-", $pair[0]);
-	$pair2 = explode("-", $pair[1]);
-
-	$start1 = (int) $pair1[0];
-	$end1 = (int) $pair1[1];
-	$start2 = (int) $pair2[0];
-	$end2 = (int) $pair2[1];
-
-	if ($start1 > $start2) {
-		$t1 = $start1;
-		$t2 = $end1;
-		$start1 = $start2;
-		$end1 = $end2;
-		$start2 = $t1;
-		$end2 = $t2;
-	}
+	[[$start1, $end1], [$start2, $end2]] = $pair;
 
 	if ($start1 <= $start2 && $end1 >= $end2) {
+		$redundant++;
+	} else if ($start2 <= $start1 && $end2 >= $end1) {
 		$redundant++;
 	}
 }
